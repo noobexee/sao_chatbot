b0338fd829cbff81b0181b2649cb2f5f960aae8b
tinna <tinnasmartlol@gmail.com>
2026-02-09 21:18:19 +0700

    fix getbaseURL() for deployment

--- apps/sao_chatbot_frontend/src/libs/config.ts
+++ apps/sao_chatbot_frontend/src/libs/config.ts
@@ -1,6 +1,7 @@
 export const getBaseUrl = () => {
+
   if (typeof window !== 'undefined') {
-    return process.env.NEXT_PUBLIC_RAG_API_URL;
+    return ''; 
   }
   
   return process.env.INTERNAL_API_URL || 'http://backend:8000';
