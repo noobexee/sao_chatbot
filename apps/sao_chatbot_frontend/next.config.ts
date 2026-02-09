40b8ba09e4140343e4bea10d49ce3ef1a8aa00c1
tinna <tinnasmartlol@gmail.com>
2026-02-09 20:59:49 +0700

    fix next config

--- apps/sao_chatbot_frontend/next.config.ts
+++ apps/sao_chatbot_frontend/next.config.ts
@@ -1,9 +1,17 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
-  output: 'standalone',
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        // When you call /api/v1/... on your frontend
+        source: '/api/v1/:path*',
+        // It proxy-passes to your backend IP
+        destination: 'http://3.224.184.102:8000/api/v1/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
